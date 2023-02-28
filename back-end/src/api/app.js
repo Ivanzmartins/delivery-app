d27b707836386f1f85850ecddac5ba13c42b3807
Ivanzmartins <ivanzmartins@gmail.com>
2023-02-28 18:29:03 -0300

    feat: rota /products e /products/:id

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const loginRouter = require('../Routes/loginRouter');
+const productsRouter = require('../Routes/productsRouter');
 
 const app = express();
 app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use('/login', loginRouter);
+app.use('/products', productsRouter);
 
 module.exports = app;
