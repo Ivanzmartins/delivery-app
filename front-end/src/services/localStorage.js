346a87f6a93faa93448652d3784c9c4f67d7c99a
Hugo <huleonardo19@gmail.com>
2023-02-28 17:33:02 -0300

    feat: cria funcs de adicionar e remover itens do carrinho no localstorage.

--- front-end/src/services/localStorage.js
+++ front-end/src/services/localStorage.js
@@ -1,8 +1,29 @@
 export const localStorageSaveItem = (key, value) => {
   if (typeof value !== 'string') {
     localStorage.setItem(key, JSON.stringify(value));
+  } else {
+    localStorage.setItem(key, value);
   }
-  localStorage.setItem(key, value);
 };
 
 export const getLocalStorageItem = (key) => JSON.parse(localStorage.getItem(key));
+
+export const addLocalStorageCartItem = (item) => {
+  const localStorageItems = getLocalStorageItem('carrinho');
+  if (!localStorageItems) {
+    localStorageSaveItem('carrinho', [item]);
+  } else {
+    localStorageSaveItem('carrinho', [...localStorageItems, item]);
+  }
+};
+
+export const rmLocalStorageCartItem = (item) => {
+  const localStorageItems = getLocalStorageItem('carrinho');
+  if (!localStorageItems) {
+    localStorageSaveItem('carrinho', []);
+  } else {
+    const itemIndex = localStorageItems.findIndex((e) => e.id === item.id);
+    const removeItem = localStorageItems.splice(itemIndex, 1);
+    localStorageSaveItem('carrinho', removeItem);
+  }
+};
