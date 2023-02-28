45d6aeff4acb529dab2a439cbda301850186de07
Ivanzmartins <ivanzmartins@gmail.com>
2023-02-28 17:46:53 -0300

    chore: mudança de nomes

--- back-end/src/Routes/UserRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const UserController = require('../Controller/UserController');
-
-
-const userRouter = express.Router();
-
-userRouter.post('/', UserController.login);
-
-module.exports = userRouter;
--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const userRouter = require('../Routes/UserRouter');
+const loginRouter = require('../Routes/loginRouter');
 
 const app = express();
-
+app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
-app.use('/login', userRouter);
+app.use('/login', loginRouter);
 
 module.exports = app;
