59a49e5422f340b1e0a2adc65936d9b1678fb075
Ivanzmartins <ivanzmartins@gmail.com>
2023-03-01 09:15:39 -0300

    chore: adiciona cors à aplicação

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const loginRouter = require('../Routes/loginRouter');
 const productsRouter = require('../Routes/productsRouter');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use('/login', loginRouter);
