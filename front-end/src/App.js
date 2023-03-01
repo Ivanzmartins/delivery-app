d12d988f011ddf4b4e8e979a000f5ad287827e06
Hugo <huleonardo19@gmail.com>
2023-03-01 17:19:38 -0300

    feat: adiciona rota "/" para renderizar a page login.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <Products /> } />
+      <Route path="/" element={ <Login /> } />
     </Routes>
   );
 }
