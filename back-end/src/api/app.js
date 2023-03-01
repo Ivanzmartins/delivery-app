c5b8b19fac342e52465da03db29cd55da0c6936d
Ivan Martins Machado Netto <104395881+Ivanzmartins@users.noreply.github.com>
2023-03-01 10:50:34 -0300

    Update app.js
    fix:  lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,7 +4,6 @@ const loginRouter = require('../Routes/loginRouter');
 const registerRouter = require('../Routes/registerRouter');
 const productsRouter = require('../Routes/productsRouter');
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,6 +12,5 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/products', productsRouter);
-
 
 module.exports = app;
