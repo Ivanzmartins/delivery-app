f1241e8df010c80c2d4030945d5501bd2242dea3
Hugo <huleonardo19@gmail.com>
2023-03-06 15:35:31 -0300

    feat: adiciona table que lista todos usuarios na page do admin.

--- front-end/src/pages/AdminMenu.js
+++ front-end/src/pages/AdminMenu.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import RegisterNewUser from '../components/RegisterNewUser';
+import UsersTable from '../components/UsersTable';
 
 export default function AdminMenu() {
   return (
     <div>
       <RegisterNewUser />
+      <UsersTable />
     </div>
   );
 }
