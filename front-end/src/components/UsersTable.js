d849496b953d15163da5c1348927ee67cb6a3841
Hugo <huleonardo19@gmail.com>
2023-03-06 15:35:09 -0300

    feat: cria table para listar todos os usuários.

---
+++ front-end/src/components/UsersTable.js
@@ -0,0 +1,71 @@
+import React, { useContext, useEffect, useState } from 'react';
+import DeliveryContext from '../context/DeliveryContext';
+
+export default function UsersTable() {
+  const [users, setUsers] = useState([]);
+  const { usersOfDB } = useContext(DeliveryContext);
+
+  const tableColumns = ['Item', 'Nome', 'E-mail', 'Tipo', 'Excluir'];
+
+  useEffect(() => {
+    const getUsers = () => {
+      setUsers(usersOfDB);
+    };
+    getUsers();
+  }, [usersOfDB]);
+
+  return (
+    <div>
+      Lista de usuários
+      <table>
+        <thead>
+          <tr>
+            {tableColumns.map((category, index) => (
+              <th key={ index }>
+                {category}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((e, index) => (
+            <tr key={ index }>
+              <td
+                data-testid={ `admin_manage__element-user-table-item-number-${index}` }
+              >
+                {e.id}
+
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-name-${index}` }
+              >
+                {e.name}
+
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-email-${index}` }
+              >
+                {e.email}
+
+              </td>
+              <td
+                data-testid={ `admin_manage__element-user-table-role-${index}` }
+              >
+                {e.role}
+
+              </td>
+              <td>
+                <button
+                  type="button"
+                  data-testid={ `admin_manage__element-user-table-remove-${index}` }
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
