99ae8aa03f5f268765b1a635bcec870ff5dfaeff
Juliana Ribeiro <silva.jurib@gmail.com>
2023-03-06 14:12:37 -0300

    feat: add orders card

---
+++ front-end/src/components/OrdersCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function OrdersCard({ id, status, date, price }) {
+  return (
+    <>
+      <div data-testid={ `customer_orders__element-order-id-${id}` }>
+        Pedido
+        {id}
+      </div>
+      <div data-testid={ `customer_orders__element-delivery-status-${id}` }>
+        {status}
+      </div>
+      <div data-testid={ `customer_orders__element-order-date-${id}` }>
+        {date}
+      </div>
+      <div data-testid={ `customer_orders__element-card-price-${id}` }>
+        {price}
+      </div>
+    </>
+  );
+}
+
+OrdersCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  status: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
+  price: PropTypes.string.isRequired,
+};
