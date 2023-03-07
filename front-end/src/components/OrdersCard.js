18a769dbc694aa262c93b289d4f913960fe48127
Juliana Ribeiro <silva.jurib@gmail.com>
2023-03-07 14:38:05 -0300

    fix: corrige formato da data

--- front-end/src/components/OrdersCard.js
+++ front-end/src/components/OrdersCard.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FOUR, FIVE, SEVEN, EIGHT, TEN } from '../util/magicNumbers';
 
 export default function OrdersCard({ id, status, date, price }) {
+  const getDate = `${date.slice(EIGHT, TEN)}/${date
+    .slice(FIVE, SEVEN)}/${date.slice(0, FOUR)}`;
+
   return (
     <>
       <div data-testid={ `customer_orders__element-order-id-${id}` }>
@@ -11,7 +15,7 @@ export default function OrdersCard({ id, status, date, price }) {
         {status}
       </div>
       <div data-testid={ `customer_orders__element-order-date-${id}` }>
-        {date}
+        {getDate}
       </div>
       <div data-testid={ `customer_orders__element-card-price-${id}` }>
         {`R$ ${price}`}
---
+++ front-end/src/util/magicNumbers.js
@@ -0,0 +1,5 @@
+export const FOUR = 4;
+export const FIVE = 5;
+export const SEVEN = 7;
+export const EIGHT = 8;
+export const TEN = 10;
