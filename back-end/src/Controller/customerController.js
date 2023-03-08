dc90d506a5aba55bd30e1432ef95e66eb8668c3e
Ivan Martins Machado Netto <104395881+Ivanzmartins@users.noreply.github.com>
2023-03-08 15:02:47 -0300

    Update customerController.js

--- back-end/src/Controller/customerController.js
+++ back-end/src/Controller/customerController.js
@@ -3,9 +3,10 @@ const customerService = require('../Service/customerService');
 const createOrder = async (req, res) => {
   const userId = req.user.id;
   const { saleInfos, products } = req.body;
-  await customerService.createOrder({ ...saleInfos, userId }, products);
-  console.log(req.user);
-  return res.sendStatus(201);
+  console.log(products, 'products');
+  const order = await customerService.createOrder({ ...saleInfos, userId }, products);
+  console.log(order);
+  return res.status(201).json({ order });
 };
 
 const getAllCustomerOrders = async (req, res) => {
