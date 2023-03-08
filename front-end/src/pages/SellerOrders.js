375466951ce91776878d5a8501f22b62a387a7b3
Juliana Ribeiro <silva.jurib@gmail.com>
2023-03-08 12:45:23 -0300

    feat: add seller orders page

---
+++ front-end/src/pages/SellerOrders.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import OrdersCard from '../components/OrdersCard';
+
+export default function Orders() {
+  const [allOrders, setAllOrders] = useState([]);
+  const [failedRequest, setFailedRequest] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const getOrders = async () => {
+    try {
+      const orders = await apiGetAll('/seller/orders/');
+      setAllOrders(orders);
+    } catch (error) {
+      setFailedRequest(true);
+      setErrorMessage('Não foi possível encontrar pedidos');
+    }
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  return (
+    <main>
+      <Header />
+      {!failedRequest ? allOrders.map((order) => (
+        <Link key={ order.id } to={ `/seller/orders/${order.id}` }>
+          <div>
+            <OrdersCard
+              id={ order.id }
+              status={ order.status }
+              date={ order.saleDate }
+              price={ order.totalPrice }
+            />
+          </div>
+        </Link>)) : (<p>{errorMessage}</p>)}
+    </main>
+  );
+}
