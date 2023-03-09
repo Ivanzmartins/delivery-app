876bdc3f5bd957187ae62f794ee81a29911cbabb
Ivan Martins Machado Netto <104395881+Ivanzmartins@users.noreply.github.com>
2023-03-09 17:24:53 -0300

    Update customerRouter.js

--- back-end/src/Routes/customerRouter.js
+++ back-end/src/Routes/customerRouter.js
@@ -16,6 +16,7 @@ customerRouter.get('/products', productsController.getAll);
 
 customerRouter.get('/products/:id', productsController.getById);
 
-customerRouter.put('/orders/:saleId/delivered', verifyToken, customerController.updateOrderToDelivered);
+customerRouter.put('/orders/:saleId/delivered',
+verifyToken, customerController.updateOrderToDelivered);
 
 module.exports = customerRouter;
